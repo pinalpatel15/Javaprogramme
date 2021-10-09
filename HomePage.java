66fe4a29465a6b1d55aed22b97d043f7883d292a
pinal patel <pinal.patel156@yahoo.com>
2021-10-09 14:34:11 +0100

    this is my first commit

---
+++ HomePage.java
@@ -0,0 +1,17 @@
+package com.nopcommerce.demo;
+
+public class HomePage {
+    //Main method to execute the program
+    public static void main(String[] args) {
+
+        //Print statement to print some text.
+        System.out.println("My First Java Program");
+        System.out.println("Learning Git");
+    }
+
+}
+
+
+
+
+
---
+++ VarName.java
@@ -0,0 +1,8 @@
+package com.nopcommerce.demo;
+
+public class VarName {
+}
+
+public static void main(String [] args){
+
+}
